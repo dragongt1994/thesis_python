56f1664934c8337fb370a34b465228aab9c7c5e1
dragongt1994 <mark8287285@yahoo.com>
2018-06-21 01:51:17 +0800

    1st Edit Of Packets. Added 1 int counter at beginning of packet and md5 at the end. Multiple packets handled by one class

--- client_machine.py
+++ client_machine.py
@@ -21,7 +21,7 @@ class messageType(Enum):
 class earthquakeData:
     def __init__(self,npts=0,delta=0,maxv=0,minv=0,val=None, f=None):
         if f!=None:
-            f=open(fname,"b")
+            self.fname=f
             temp=f.read()
             self.npts=struct.unpack('L',temp[0:4])
             self.delta=struct.unpack('f',temp[4:8])
@@ -60,12 +60,31 @@ class Packet:
         if p==None:
             l=len(dat)
             self.__dat=dat
-            print(l)
-            temp=bytes([0]*(4060-l))
-            temp2=struct.pack('i',l)+dat+temp
-            s=md5(temp2)
-            s2=str.encode(s)
-            self.__pck=temp2+s2
+            res=[]
+            
+            nmb=math.ceil(l/4096)
+            print("nmb"+ str(nmb))
+            for ctr in range(0, nmb):
+                pck_temp=self.dat[ctr*4060:(ctr+1)*4060]
+                tdat=pck_temp
+                l=len(tdat)
+                print("4060-l="+str(4060-l))
+                temp=bytes([0]*(4060-l))
+                temp2=struct.pack('i',ctr)+tdat+temp
+                s=md5(temp2)
+                s2=str.encode(s)
+                temp2=temp2+s2
+                
+                res=res+[temp2]
+            
+            self.__pck=res
+
+#            ftemp=open("pack_"+str(ctr)+".dat", "wb")
+#            ftemp.write(tdat)
+#            ftemp.close()
+#            
+            
+            
         else:
             self.__dat=self.__pck[4:4064]
             self.__pck=p
@@ -83,7 +102,10 @@ class Packet:
 class clientMachine:
     def __init__(self, username,fname,mname, lname, currData=None):
         self.username=username
-        self.sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.fname=fname
+        self.mname=mname
+        self.lname=lname
+    #    self.sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         self.__currData=currData
     @property
     def currData(self):
@@ -102,32 +124,51 @@ class clientMachine:
         max_length=4
         
     def askConnection(self, host, port):
-        self.sck.connect(host, port)
+    #    self.sck.connect(host, port)
         #username,fname,mname,lname 30 for whole
-        l=len(self.__currData)
-        nmb=math.ceil(l/4096)
-        dat=[[messageType.CONN_REQT]+str.encode(self.username)+ str.encode(self.fname)+ str.encode(self.mname)+str.encode(self.lname)+struct.pack('L', l)]
-        p=Packet(dat)
-        self.sck.send(p.pck())
-        r=self.sck.recv(4096)
-        temp=Packet(p=r).dat
-        if temp==messageType.CONN_CONF:
-            for ctr in range(0, nmb):
-                tdat=self.currData.packData()[ctr*4096:(ctr+1)*4096]
-                self.sck.send(tdat.packData)
+        temp=self.__currData.packData()
+        l=len(temp)
+ #       nmb=math.ceil(l/4096)
+        #dat=struct.pack('I', messageType.CONN_REQT.value)+str.encode(self.username)+ str.encode(self.fname)+ str.encode(self.mname)+str.encode(self.lname)+struct.pack('I', l)
+      #  p=Packet(dat)
+  #      self.sck.send(p.pck())
+     #   r=self.sck.recv(4096)
+ #       temp=Packet(p=r).dat
+    #    if temp==messageType.CONN_CONF:
+        print("l="+str(l))
+        pck_temp=Packet(temp)
+        tot_dat=pck_temp.pck
+        ctr=0
+        for tdat in tot_dat:
+            ftemp=open("pack_dat/pack_"+str(ctr)+".dat", "wb")
+            ftemp.write(tdat)
+            ftemp.close()
+            ctr=ctr+1
+            
+            
+        
+        return tot_dat
+        #ftemp=open("pack_"+str(ctr)+".dat", "wb")
+        #ftemp.write(tdat)
+        #ft    emp.close()
+    #            self.sck.send(tdat.packData)
             
             
             
             
     
 a=[]    
-for i in range(0, 203):
+for i in range(0, 2000):
     a=a+[float(i)]
     
-
+print(type(messageType.CONN_CONF.value))
 eq=earthquakeData(len(a), 0.01, 1,-1, a)
-eq.saveToFile("earth.dat")
 
+b=clientMachine("user1", "mark", "raz", "ochoa", eq)
+t=b.askConnection("127.1.1.9", "3000")
+
+#eq.saveToFile("earth.dat")
+#eq
 
 #if __name__ == "__main__":
   #  main()
